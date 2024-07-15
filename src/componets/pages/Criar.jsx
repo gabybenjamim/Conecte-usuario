2e62dffc800f7ee21e9eb5bdbe5b3f8e791b3eca
gabysenac <maria.gbenjamim@senacsp.edu.br>
2024-07-15 16:56:09 -0300

    arquivo certo

--- src/componets/pages/Criar.jsx
+++ src/componets/pages/Criar.jsx
@@ -1,7 +1,37 @@
+import { useNavigate } from 'react-router-dom'
+
 import CriarForm from '../CriarForm/CriarForm'
 import styles from './Criar.module.css'
 
+
+
+
+
+
+
 function Criar() {
+    const navigate = useNavigate()
+function createPost(usuario) {
+    // inicializar cost and services
+    
+    usuario.email = 0
+    usuario.senha = []
+
+    .fetch("http://localhost:5000/usuarios", {
+        method: 'GET',
+        headers: {
+            'Context-Type': 'application/json',
+        },
+    }) 
+}
+
+
+
+
+
+
+
+
     return (
         <div className={styles.criar_container}>
             <h1>Criar Conta</h1>
